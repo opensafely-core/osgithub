7f4610d9ecbfbc3650edf1df3348510a4216b6d2
Becky Smith <becky.smith@thedatalab.org>
2021-08-26 12:08:32 +0100

    fix: Name the package properly

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ with open(os.path.join("osgithub", "VERSION")) as f:
     version = f.read().strip()
 
 setup(
-    name="github-api-cache",
+    name="osgithub",
     version=version,
     packages=find_packages(exclude=["tests"]),
-    url="https://github.com/opensafely-core/github-api-cache",
+    url="https://github.com/opensafely-core/osgithub",
     author="OpenSAFELY",
     author_email="tech@opensafely.org",
     python_requires=">=3.7",
